a0f693c40adacaf2976b50e6c5884d358218704a
tansenkhan1990 <tansenkhan1990@gmail.com>
2017-12-10 20:49:48 +0100

    Insert of data in database

--- crud.php
+++ crud.php
@@ -8,10 +8,17 @@ class Crud{
 
     }
 
-    public function insert($name,$email,$password,$address,$mobile)
-    {
-
-    }
+   public function insert($name,$email,$password,$address,$mobile)
+   {
+       $sql = "INSERT INTO info (name, email,password,address,mobile)
+VALUES ('$name','$email','$password','$address','$mobile')";
+       if ($this->conn->query($sql) === TRUE) {
+           echo "New record created successfully";
+       } else {
+           echo "failed";
+       }
+   }
 }
 $obj=new Crud();
+$obj->insert('tansen','tansenkhan1990@gmail.com','bonn','bonn','0152');
 ?>
