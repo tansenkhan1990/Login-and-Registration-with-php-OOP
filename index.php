b93a1cf5554bfd57ee6534f798774c3c139ab816
tansenkhan1990 <tansenkhan1990@gmail.com>
2017-12-14 14:43:44 +0100

    final modification of insert form

--- index.php
+++ index.php
@@ -1,33 +1,40 @@
 <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <?php
-
 include 'crud.php';
 include 'navbar.php';
 if(isset($_REQUEST['signup']))
 {
-    extract($_REQUEST);
-    if($obj->insert($name,$email,$password,$mobile))
-    {
-        $mess='insert successfull';
-    }
-    else{
-        $mess='failed to insert';
-    }
+extract($_REQUEST);
+if ($obj->insert($name, $email, $password, $mobile)) {
+    $mess = "account create successfully";
+}
+else{
+    $mess="faild to create account";
 }
 ?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+      integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+        crossorigin="anonymous"></script>
 <div class="container">
     <br><br>
 
     <div class="row">
-        <center><div class="alert alert-success">
-            <strong>Congrats</strong> account create successfully
+        <center>
+            <div class="alert alert-success">
+                <strong><?php
+                    echo $mess;
+                    }
+                ?>
+                </strong>
         </div></center>
 
         <div class="col-md-6"><form class="form-horizontal" action="index.php">
