1651328ad705310f2f24539973c42ed213d2f02d
tansenkhan1990 <tansenkhan1990@gmail.com>
2017-12-16 15:50:45 +0100

    Adding table and shows all informations

--- profile.php
+++ profile.php
@@ -26,67 +26,51 @@ else {
         </head>
         <body>
 
-        <nav class="navbar navbar-default">
+        <nav class="navbar navbar-inverse">
             <div class="container-fluid">
                 <div class="navbar-header">
-                    <a class="navbar-brand" href="#">Welcome <?php echo $name; }?></a>
+                    <a class="navbar-brand" href="#">Welcome</a>
                 </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#"><?php echo $name;?></a></li>
 
+
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Update Profile</a></li>
+                    <li><a href="#"> singout</a></li>
+                </ul>
             </div>
         </nav>
 
         <div class="container">
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <title>Bootstrap Example</title>
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-            </head>
-            <body>
+            <h2>Hello <?php echo $name;?></h2>
+            <p>Your basic information is given belows</p>
+            <table class="table table-condensed">
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Mobile</th>
+                    <th>Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td><?php echo $name;?></td>
+                    <td><?php echo $mobile;?></td>
+                    <td><?php echo $email;?></td>
+                </tr>
 
-            <div class="container">
-                <h2><?php echo "$name your basic information is given below";?></h2>
-
-                <table class="table">
-                    <thead>
-                    <tr>
-                        <th>Firstname</th>
-                        <th>Lastname</th>
-                        <th>Email</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr>
-                        <td>John</td>
-                        <td>Doe</td>
-                        <td>john@example.com</td>
-                    </tr>
-                    <tr>
-                        <td>Mary</td>
-                        <td>Moe</td>
-                        <td>mary@example.com</td>
-                    </tr>
-                    <tr>
-                        <td>July</td>
-                        <td>Dooley</td>
-                        <td>july@example.com</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-
-            </body>
-            </html>
-
+                </tbody>
+            </table>
         </div>
+
 
         </body>
         </html>
+
         <?php
     }
+}
 
 ?>
