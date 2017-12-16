0184544845a86c34dba6bc1888ffad8629229d6e
tansenkhan1990 <tansenkhan1990@gmail.com>
2017-12-16 15:22:01 +0100

    redirect in homepage without login

--- profile.php
+++ profile.php
@@ -1,41 +1,48 @@
 <?php
+include 'crud.php';
 session_start();
 if(!isset($_SESSION['email'])){
 
     header("Location: index.php");
+
 }
-else
-{
-    ?>
+else {
+    $email = $_SESSION['email'];
+    $read = $obj->Read($email);
+    foreach ($read as $re) {
+        extract($re);
 
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <title>Bootstrap Example</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    </head>
-    <body>
+        ?>
 
-    <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">Welcome <?php echo $name;?></a>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <title>Bootstrap Example</title>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        </head>
+        <body>
+
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">Welcome <?php echo $name; }?></a>
+                </div>
+
             </div>
+        </nav>
 
+        <div class="container">
+            <h3>Basic Navbar Example</h3>
+            <p>A navigation bar is a navigation header that is placed at the top of the page.</p>
         </div>
-    </nav>
 
-    <div class="container">
-        <h3>Basic Navbar Example</h3>
-        <p>A navigation bar is a navigation header that is placed at the top of the page.</p>
-    </div>
+        </body>
+        </html>
+        <?php
+    }
 
-    </body>
-    </html>
-<?php
-}
 ?>
