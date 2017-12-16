c141c7b76fdae0d15b2383f086faaaa1ccc72d95
tansenkhan1990 <tansenkhan1990@gmail.com>
2017-12-16 14:45:29 +0100

    adding new navbar

--- profile.php
+++ profile.php
@@ -1,5 +1,4 @@
 <?php
-include 'navbar2.php';
 session_start();
 if(!isset($_SESSION['email'])){
 
@@ -7,6 +6,36 @@ if(!isset($_SESSION['email'])){
 }
 else
 {
-    echo 'welcome to your profile';
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <title>Bootstrap Example</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">Welcome <?php echo $name;?></a>
+            </div>
+
+        </div>
+    </nav>
+
+    <div class="container">
+        <h3>Basic Navbar Example</h3>
+        <p>A navigation bar is a navigation header that is placed at the top of the page.</p>
+    </div>
+
+    </body>
+    </html>
+<?php
 }
 ?>
