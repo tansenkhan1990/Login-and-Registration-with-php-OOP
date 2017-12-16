5e5714d52694c38f2d0effee7196acf5399a42e6
tansenkhan1990 <tansenkhan1990@gmail.com>
2017-12-16 15:26:55 +0100

    Adding table

--- profile.php
+++ profile.php
@@ -36,8 +36,52 @@ else {
         </nav>
 
         <div class="container">
-            <h3>Basic Navbar Example</h3>
-            <p>A navigation bar is a navigation header that is placed at the top of the page.</p>
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <title>Bootstrap Example</title>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+            </head>
+            <body>
+
+            <div class="container">
+                <h2><?php echo "$name your basic information is given below";?></h2>
+
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Email</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>John</td>
+                        <td>Doe</td>
+                        <td>john@example.com</td>
+                    </tr>
+                    <tr>
+                        <td>Mary</td>
+                        <td>Moe</td>
+                        <td>mary@example.com</td>
+                    </tr>
+                    <tr>
+                        <td>July</td>
+                        <td>Dooley</td>
+                        <td>july@example.com</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            </body>
+            </html>
+
         </div>
 
         </body>
